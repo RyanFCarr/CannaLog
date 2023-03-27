7f451cb8433ebe46e336c7f6e97eaa028cd87862
Raitono <bpaintx2@gmail.com>
2023-03-26 20:16:58 -0400

    .

--- src/components/pages/Plants/PlantList.tsx
+++ src/components/pages/Plants/PlantList.tsx
@@ -51,7 +51,7 @@ const PlantList: React.FC = () => {
   return (
     <Table
       data={plants}
-      columnHeaders={["Name", "Strain", "Status", "Age"]}
+      columnHeaders={["Name", "Strain", "Status"]}
       onRowClick={(plant: Plant) => navigate(`/plants/${plant.id}`)}
     />
   );
